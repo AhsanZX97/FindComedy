c1c3d3bc0b08442ef239efe119f9b383de446498
degreatahsan <degreatahsan@gmail.com>
2026-06-21 21:42:41 +0100

    fix: update sitemap filename to sitemap_index.xml in robots.txt and Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -62,7 +62,7 @@ function writeSitemap(dist: string, siteUrl: string, nights: ComedyNight[]): voi
     .map((p) => `  <url><loc>${siteUrl}${p}</loc><lastmod>${lastmod}</lastmod></url>`)
     .join('\n')
   const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${body}\n</urlset>\n`
-  writeFileSync(resolve(dist, 'sitemap.xml'), xml)
+  writeFileSync(resolve(dist, 'sitemap_index.xml'), xml)
   console.log(`[seo] wrote sitemap with ${staticPaths.length + nightPaths.length + areaPaths.length} urls`)
 }
 
